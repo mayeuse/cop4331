4dd2ea4cfe9a4df0495c20db0dfb750a4547504e
mayeuse <ma369059@ucf.edu>
2024-10-29 12:15:43 -0400

    pseudo API for login

--- src/server/express/restApi.ts
+++ src/server/express/restApi.ts
@@ -23,6 +23,30 @@ app.get(`/api/v1/version`, (req: Request, res: Response) => {
   res.send(respObj);
 });
 
+app.get('/api/v1/login', async (req, res, next) =>
+  {
+    // incoming: login, password
+    // outgoing: id, firstName, lastName, error
+    var error = '';
+    const { login, password } = req.body;
+    var id = -1;
+    var fn = '';
+    var ln = '';
+    if( login.toLowerCase() == 'rickl' && password == 'COP4331' )
+    {
+      id = 1;
+      fn = 'Rick';
+      ln = 'Leinecker';
+    }
+    else
+    {
+      error = 'Invalid user name/password';
+    }
+    var ret = { id:id, firstName:fn, lastName:ln, error:error};
+    res.status(200).json(ret);
+  });
+  
+
 app.use(express.static("./.local/vite/dist"));
 
 export default app;
