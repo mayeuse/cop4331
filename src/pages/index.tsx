c1e7cbde02e6726c2fd5cc25e4476a42a632ee41
Omenaga <justin.wu000@gmail.com>
2024-11-04 18:35:18 -0500

    Imported Login and Register from Small Project and switch Tabs with buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,16 +14,112 @@ const HomePage = (): JSX.Element => {
     setData(data);
   }
 
+  function Tabs()
+  {
+    const [content, setContent] = useState(<LoginBody />);
+
+    function RegisterOn()
+    {
+      setContent(<RegisterBody />)
+    }
+
+    function LoginOn()
+    {
+      setContent(<LoginBody />)
+    }
+
+    return (
+      <div>
+        <button className={styles.button} onClick={RegisterOn}>
+          Register
+        </button>
+        <button className={styles.button} onClick={LoginOn}>
+          Login
+        </button>
+
+        {content}
+      </div>
+    )
+  }
+
+  function RegisterBody()
+  {
+    return (
+      <div className={styles.wrapper}>
+      <h2>Sign Up</h2>
+      <form id="register">
+          <div className={styles.inputcombo}>
+              <input className={styles.inputbox} type="text" name="first_name" placeholder="First Name"/>
+          </div>
+
+          <div className={styles.inputcombo}>
+              <input className={styles.inputbox} name="last_name" placeholder="Last Name"/>
+          </div>
+
+          <div className={styles.inputcombo}>
+              <input className={styles.inputbox} name="username" placeholder="Username"/>
+          </div>
+
+          <div className={styles.inputcombo}>
+              <input className={styles.inputbox} type="password" name="password" placeholder="Password"/>
+          </div>
+          
+          <div className={styles.submitwrap}>
+              <button className={styles.submitbox} type="button" id="submit_register">Submit</button>
+          </div>
+          <div className={styles.errormessage}></div>
+      </form>
+    </div>
+    )
+  }
+
+  function LoginBody()
+  {
+    return (
+      <div className={styles.wrapper}>
+      <h2>Login</h2>
+      <form name="login">
+          <div className={styles.inputcombo}>
+              <input className={styles.inputbox} type="text" id="loginName" name="username" placeholder="Username"/>
+          </div>
+
+          <div className={styles.inputcombo}>
+              <input className={styles.inputbox} type="password" id="loginPassword" name="password" placeholder="Password"/>
+          </div>
+          
+          <div className={styles.submitwrap}>
+              <button className={styles.submitbox} type="button" id="loginButton">Submit</button>
+          </div>
+
+          <span id="loginResult"></span>
+      </form>
+    </div>
+    )
+  }
+
+  function Test()
+  {
+    return(
+      <div>
+        <img src="/images/nasa-logo.svg" alt="nasa logo" />
+        <button className={styles.button} onClick={getDataFromServer}>
+          Access server using proxy
+        </button>
+        <p>data : {data?.version}</p>
+
+      </div>
+    )
+  }
+
   return (
     <div className={styles.app}>
-      <img src="/images/nasa-logo.svg" alt="nasa logo" />
-      <button className={styles.button} onClick={getDataFromServer}>
-        Access server using proxy
-      </button>
-      <p>data : {data?.version}</p>
-      
+      <Test />      
+      <Tabs />
+      {/* <RegisterBody />
+      <LoginBody /> */}
     </div>
   );
+    
 };
 
 export default HomePage;
