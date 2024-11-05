27b50b16f9fc8848cf58ae2dd337afd7d997a14d
Ro-m-an <roman.g.rosario@gmail.com>
2024-11-05 11:49:48 -0500

    Revert "Merge branch 'newmain' of https://github.com/mayeuse/cop4331 into newmain"
    This reverts commit 6623dbe4d3c95e6f05084df9a802133f1795742d, reversing
    changes made to c1e7cbde02e6726c2fd5cc25e4476a42a632ee41.

--- src/server/express/restApi.ts
+++ src/server/express/restApi.ts
@@ -2,7 +2,6 @@ import packageJSON from "../../../package.json";
 import express, { Application } from "express";
 import cors from "cors";
 import { Request, Response } from "express";
-import { Collections } from "./mongo";
 
 const app: Application = express();
 
@@ -15,36 +14,25 @@ app.get("/api/v1/health", (req, res) => {
   res.send({ status: "ok" });
 });
 
-app.post('/api/v1/login', async (req, res, next) =>
-  {
-    // incoming: login, password
-    // outgoing: id, name, error
-    let error = '';
-    const { login, password } = req.body;
-    const results = await (await Collections.UserData.find({ username: login, password: password })).toArray();
-    let id = '';
-    let name = '';
-    if( results.length > 0 )
-    {
-      id = results[0]._id.toHexString();
-      name = results[0].name;
-    }
-    var ret = { id:id, name:name, error:''};
-    res.status(200).json(ret);
-  });
-  
-app.post('/api/v1/register', async (req, res, next) =>
-  {
-    // incoming: name, email, login, password
-    // outgoing: id
-    let error = '';
-    const { name, email, login, password } = req.body;
-    const results = await (await Collections.UserData.insert({ name: name, email: email, username: login, password: password, badges: [], exerciseLog: [], goals: {}}));
-    var ret = {id:results.insertedId.toHexString()};
-    res.status(200).json(ret);
-  });
 
+app.get("/api/v1/yourendpoint", handle_yourendpoint)
+
+app.get(`/api/v1/version`, (req: Request, res: Response) => {
+  const respObj: RespExampleType = {
+    id: 1,
+    version: packageJSON.version,
+    envVal: process.env.ENV_VALUE as string, // sample server-side env value
+  };
+  res.send(respObj);
+});
 
 app.use(express.static("./.local/vite/dist"));
 
 export default app;
+
+
+function handle_yourendpoint(req: Request, res: Response) {
+  res.send({
+    "cool": "thanks"
+  })
+}
