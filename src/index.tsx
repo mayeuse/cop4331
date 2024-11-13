a91de29995aafb5b2c88f2f116d85fade458068e
cbrandt77 <84159639+cbrandt77@users.noreply.github.com>
2024-11-13 11:59:30 -0500

    Deprecation fix: Use React18 method of mounting instead of React17

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 // don't worry about this
 
 import React from "react";
-import ReactDOM from "react-dom";
 import "@/styles/global.css";
 import App from "@/App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
 );
